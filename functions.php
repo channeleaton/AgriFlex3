92a6fe60c9b916760a8c59a11bf32f75789682c1
Zachary Watkins <Zachary.Watkins@ag.tamu.edu>
2015-05-18 10:57:38 -0500

    Add Background Image customization feature to theme.

--- functions.php
+++ functions.php
@@ -78,3 +78,56 @@ class AgriFlex {
 }
 
 AgriFlex::get_instance();
+
+
+
+/**
+ * Theme Option for Background Images
+ * https://codex.wordpress.org/Theme_Customization_API
+ * @since 1.0
+ */
+function agriflex_customize_register($wp_customize){
+	// A group of options for the control
+	$wp_customize->add_section(
+    'agriflex_background_options',
+    array(
+        'title'     => 'Background Image',
+        'priority'  => 30 
+    )
+	);
+	// Register the option's data for the control
+	$wp_customize->add_setting(
+    'agriflex_background_image',
+    array(
+        'default'      => '',
+        'transport'    => 'refresh'
+    )
+	);
+	// Display a control in Appearance > Customize
+	$wp_customize->add_control(
+    new WP_Customize_Image_Control(
+      $wp_customize,
+      'agriflex_background_image',
+      array(
+        'label'    => '',
+        'settings' => 'agriflex_background_image',
+        'section'  => 'agriflex_background_options'
+      )
+    )
+	);
+}
+add_action('customize_register', 'agriflex_customize_register');
+
+// Use the setting's content in the theme.
+function agriflex_customize_css() {
+	?>
+		<style type="text/css">
+			<?php if ( get_theme_mod( 'agriflex_background_image' ) != '' && 0 < count( strlen( ( $background_image_url = get_theme_mod( 'agriflex_background_image' ) ) ) ) ) { ?>
+	    	body {
+  	  		background-image: url(<?php echo $background_image_url; ?>);
+    		}
+			<?php } // end if ?>
+		</style>
+	<?php
+}
+add_action( 'wp_head', 'agriflex_customize_css');
